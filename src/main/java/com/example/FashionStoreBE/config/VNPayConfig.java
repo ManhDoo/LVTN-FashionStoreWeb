29c92129905856eec7adc49c6181de38871ff641
manh <manh@gmail.com>
2025-07-21 16:42:00 +0700

    config vnpay

--- src/main/java/com/example/FashionStoreBE/config/VNPayConfig.java
+++ src/main/java/com/example/FashionStoreBE/config/VNPayConfig.java
@@ -4,7 +4,7 @@ public class VNPayConfig {
     public static final String VNP_TMN_CODE = "RFHY9H6E"; // Provided by VNPay
     public static final String VNP_HASH_SECRET = "J7AT93QRG2CKM07V7BWBVRI2PBL9XBE9"; // Provided by VNPay
     public static final String VNP_URL = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static final String VNP_RETURN_URL = "https://3399efe052cc.ngrok-free.app/api/vnpay/return";
+    public static final String VNP_RETURN_URL = "https://lvtn-fashionstoreweb.onrender.com/api/vnpay/return";
     public static final String VNP_VERSION = "2.1.0";
     public static final String VNP_COMMAND = "pay";
     public static final String VNP_ORDER_TYPE = "billpayment";
